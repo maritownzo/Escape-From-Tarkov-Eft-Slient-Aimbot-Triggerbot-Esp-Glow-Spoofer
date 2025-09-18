216c3c026762904c21e3b9beee540aebc7474613
slit2468 <paying1965@outlook.com>
2025-09-18 12:00:21 +0400

    Upload Files

---
+++ EFT-External-Cheat/hacks/esp_loot.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstring>
+
+static inline bool IsPlayerCorpse( const char *string )
+{
+    return !strncmp( string, "Observed_Player", 15 );
+}
+
+static inline bool IsGoodLoot( const char *string )
+{
+    bool isGood = false;
+    isGood |= (bool)strstr( string, "_transilluminator" );
+    //isGood |= (bool)strstr( string, "key_" );
+    isGood |= (bool)strstr( string, "gl40" );
+    isGood |= (bool)strstr( string, "lab" );
+    isGood |= (bool)strstr( string, "info_intelligence" );
+    isGood |= (bool)strstr( string, "video_card" );
+    isGood |= (bool)strstr( string, "barter_electr_tetriz" );
+
+    return isGood;
+}
