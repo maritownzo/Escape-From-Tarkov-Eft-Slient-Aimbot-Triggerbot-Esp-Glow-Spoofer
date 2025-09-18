33da849b84f3ef0fb784888439d6341fcf2251fb
slit2468 <paying1965@outlook.com>
2025-09-18 12:00:06 +0400

    Upload Files

---
+++ EFT-External-Cheat/globals.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "sdk/unity.h"
+#include "vmread/hlapi/hlapi.h"
+#include <vector>
+#include <cstdint>
+#include <deque>
+#include <map>
+#include <atomic>
+#include <linux/input-event-codes.h>
+
+inline WinProcess *process;
+inline uintptr_t unityplayerBase;
+inline uintptr_t monoBase;
+inline uintptr_t monoRootDomain;
+inline uintptr_t gomAddr;
+inline GlobalObjectManager gom;
+inline uintptr_t gameWorldAddr;
+inline uintptr_t cameraAddr;
+inline uintptr_t localPlayerAddr;
+inline Vector3D localPlayerHead;
+inline bool localPlayerIsAiming;
+inline bool localPlayerIsOpticCamera;
+inline CameraEntity cameraEntity;
+
+inline bool pressedKeys[500];
+inline std::atomic<bool> running = true;
+inline bool mainLoopDone = true;
